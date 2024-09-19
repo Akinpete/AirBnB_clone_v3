b0e94c2dcd3de5595a31a22b502a7e87e9e273d2
Akinpete <peter.akinpelumi@lifesci.uniben.edu>
2024-09-19 14:59:05 +0100

    PEP 8

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,6 +5,7 @@ views for home/index page
 from flask import Flask, Blueprint, jsonify
 from api.v1.views import app_views
 
+
 @app_views.route('/status', strict_slashes=False)
 def hbnbStatus():
     """hbnbStatus"""
