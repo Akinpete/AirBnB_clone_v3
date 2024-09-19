3fca0fe476c6674763904270395608b7d5ac0ea4
Akinpete <peter.akinpelumi@lifesci.uniben.edu>
2024-09-19 09:32:28 +0100

    adding two new methods

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -33,6 +33,25 @@ class FileStorage:
                     new_dict[key] = value
             return new_dict
         return self.__objects
+    
+    def get(self, cls, id):
+        """Retrieve an object"""
+        if cls in classes.values():
+            key = cls.__name__ + "." + id
+            obj = self.__objects.get(key)
+        return obj
+    
+    def count(self, cls=None):
+        """Count the number of objects in storage matching the given class.
+        If no class is passed, return the count of all objects in storage."""
+        if cls:
+            all_objs = self.all(cls)
+            count = len(all_objs)
+        else:
+            count = len(self.__objects)
+        return count
+            
+        
 
     def new(self, obj):
         """sets in __objects the obj with key <obj class name>.id"""
